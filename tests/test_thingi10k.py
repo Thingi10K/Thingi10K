d29c38c585d9c52641a9ed11f9ea45ca7525645b
Qingnan Zhou <qzhou@adobe.com>
2025-07-19 12:55:44 -0400

    Update unit test.

--- tests/test_thingi10k.py
+++ tests/test_thingi10k.py
@@ -1,8 +1,15 @@
 import pytest
 import thingi10k
+from pathlib import Path
 
 def test_thingi10k():
     """ Simple test to ensure everything is working """
     thingi10k.init()
-    high_genus_files = [entry['file_id'] for entry in thingi10k.dataset(genus=(1000, None))]
-    assert len(high_genus_files) == 3
+    high_genus_dataset = thingi10k.dataset(genus=(1000, None))
+    assert len(high_genus_dataset) == 3
+
+    for entry in high_genus_dataset:
+        assert Path(entry['file_path']).exists()
+        V, F = thingi10k.load_file(entry['file_path'])
+        assert V.shape[0] == entry['num_vertices']
+        assert F.shape[0] == entry['num_facets']
